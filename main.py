f8f2156b2c85b66d776b7a2a1d33161884b5d2c9
meta-byte <45606335+meta-byte@users.noreply.github.com>
2024-11-14 16:29:16 -0700

    allow player sprite movement

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame
 from constants import *
-import pygame
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -14,8 +22,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
+        
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
